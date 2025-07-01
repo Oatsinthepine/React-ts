f5c3bffe71fbd19c3cec9dd0302e89aeafd3bd51
Oatsinthepine <zzy20001201@gmail.com>
2025-07-01 13:01:45 +1000

    Add event handling.

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,17 @@
 // remember in React, a component cannot return more than one element, to deal with this, we can use a fragment or a parent element like a div
 import { Fragment } from 'react';
+import type {MouseEvent} from "react";
 
 function ListGroup() {
     // this is a practice of render lists
     let items = ['An item', 'A second item', 'A third item', 'A fourth item', 'And a fifth one'];
+
+    // this is a practice of event handling
+    const handleClick = (event: MouseEvent) => {
+        console.log(event)
+    }
+
+
     // items = []; // uncomment this line to test conditional rendering
     // conditional rendering
     if (items.length === 0) {
@@ -24,7 +32,16 @@ function ListGroup() {
             {/*we need to wrap js/ts code using '{}' in between html blocks*/}
             {items.map((item) => {
                 // list items should have a unique key property so that React can identify which items have changed, are added, or are removed dynamically
-                return (<li key={item}>{item}</li>)
+                // return (<li className="list-group-item"  key={item} onClick={() => {return console.log(`Clicked ${item}`)}}>{item}</li>)
+                // this is another way to write the same code, by setting the event handler separately
+                return (
+                    <li className="list-group-item"
+                        key = {item}
+                        onClick={handleClick}>
+                        {item}
+                    </li>
+                )
+
             })}
         </ul>
         </Fragment>
